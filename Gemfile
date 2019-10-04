146b209bafb840c459d5d6094aa0acf210da86cc
Chang Calvin <chang.wen-lung@auto1.com>
2019-10-04 12:16:08 +0200

    [Init] Project init

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'cocoapods', '1.8.1'
+gem 'fastlane', '2.123.0'
+gem 'xcode-install'
+
+# gem "rails"
---
+++ PurchaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PurchaseApp
+//
+//  Created by Chang Wen-Lung on 04.10.19.
+//  Copyright © 2019 Acclegor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
